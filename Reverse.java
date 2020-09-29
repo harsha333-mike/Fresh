9a470ed51b6814b2e19a6dfbbe1aa93949251a65
harsha333-mike <71881406+harsha333-mike@users.noreply.github.com>
2020-09-29 16:46:53 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse
+{
+static String s = "hello";
+static public String rev = "";
+    public static void main(String[]args)
+     {
+     	for( int a = s.length()-1 ; a>=0 ; a --)
+	{
+		rev = rev + s.charAt(a);
+	}
+	System.out.println(rev);
+      }
